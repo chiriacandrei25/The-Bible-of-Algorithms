76a25e003e3a5dd6836a6b96bfa5378a2b288b9e
chiriacandrei25 <chiriacandrei25@yahoo.com>
2019-12-05 15:49:06 +0200

    Update Prime Number Test.cpp

--- Prime Number Test.cpp
+++ Prime Number Test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cmath> /// We need the "cmath" library for the sqrt function
 
 using namespace std;
 
 bool isPrime(int n) {
     if(n <= 1)
         return false;
-    for(int i = 2; i < n; i++) {
+    for(int i = 2; i < sqrt(n); i++) {
         if(n % i == 0)
             return false;
     }
