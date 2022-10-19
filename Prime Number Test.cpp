d64e2d6a23afe84eb039796ee3a9a6057a6e1a54
anoshkekal <97592173+anoshkekal@users.noreply.github.com>
2022-10-19 13:11:20 +0530

    Update Prime Number Test.cpp
    in this for(int i = 2; i < sqrt(n); i++) loop when lets say n was 9 the sqrt of n is 3.
    3 is not less than 3 so it would return 1 which is wrong. 
    as we are using int here it will floor the value if we get decimals.
    eg: 137 sqrt is 11.7046  = 11 as it is a integer data type value.

--- Prime Number Test.cpp
+++ Prime Number Test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool isPrime(int n) {
     if(n <= 1)
         return false;
-    for(int i = 2; i < sqrt(n); i++) {
+    for(int i = 2; i <= sqrt(n); i++) {
         if(n % i == 0)
             return false;
     }
